eada984c0670bccb7db22ad329b750b1891b5f69
dsokhna <nianemouhamed100@gmail.com>
2024-01-31 00:36:26 -0500

    Ajout du menu

--- app/src/main/java/uqac/dim/nitflex/MainActivity.java
+++ app/src/main/java/uqac/dim/nitflex/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
     public void afficherStart(View v){
         Log.i("DIM", "La video a commencé!");
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // https://stackoverflow.com/questions/76334499/error-constant-expression-required-android-studio
+        int id = item.getItemId();
+        if (id == R.id.menu_preferences)
+            return faireToast("Sélection des préférences");
+        return super.onOptionsItemSelected(item);
+    }
+    private boolean faireToast(String message){
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
